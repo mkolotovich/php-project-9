ecc860e9c3fc6bc7b09ebbbde0e1c4414e3d4e48
Mike Kolotovich <k2punk@gmail.com>
2025-06-04 10:30:04 +0300

    finish second step

--- public/index.php
+++ public/index.php
@@ -3,11 +3,21 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
-});
+    $params = [
+        'errors' => [],
+    ];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+})->setName('main');
+
 $app->run();
