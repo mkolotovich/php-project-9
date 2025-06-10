dd2c048159c84da6d12037b217b08aed481c52cf
Mike Kolotovich <k2punk@gmail.com>
2025-06-10 14:00:30 +0300

    edit build.sh file

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Подождём пока запустится postgres
+until pg_isready -d $DATABASE_URL -p 5432; do
+  echo "Waiting for PostgreSQL..."
+  sleep 1
+done
+
+echo "PostgreSQL is ready!"
 psql -a -d $DATABASE_URL -f database.sql
