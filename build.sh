10b6cc8cafeee1721fcdedf8da0848263a0aaa07
Mike Kolotovich <k2punk@gmail.com>
2025-06-10 16:36:20 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Подождём пока запустится postgres
-until pg_isready -d $DATABASE_URL -p 5432; do
+DB_HOST=$(echo "$DATABASE_URL" | awk -F'[/:@]' '{print $4}')
+DB_PORT=5432 # Предполагаем стандартный порт, можно изменить исходя из переменной окружения
+DB_NAME=$(echo "$DATABASE_URL" | awk -F'/' '{print $NF}') # Получаем имя БД из конца строки
+
+until pg_isready -h "$DB_HOST" -p "$DB_PORT"; do
   echo "Waiting for PostgreSQL..."
   sleep 1
 done
 
 echo "PostgreSQL is ready!"
-psql -a -d $DATABASE_URL -f database.sql
+psql -a -d "$DATABASE_URL" -f database.sql
